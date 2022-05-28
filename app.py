2e0f6ea4f20092c382dc20161b258e70a385b362
21618 <21618@ecam.be>
2022-05-28 11:35:16 +0200

    post + follows + likes

--- app.py
+++ app.py
@@ -1,15 +1,102 @@
-from flask import Flask
-from flask import request
-import sqlite3
+from flask import Flask, jsonify, session, request
+import base64
+from db import *
 
 app = Flask(__name__)
+app.secret_key = "DPYMcFt7gaMU8hq3hvfqAsJ6C5gy9ZJJ"
 
 @app.route("/register", methods=["POST"])
 def register():
     username = request.json["username"]
     password = request.json["password"]
-    return ""
-    
+
+    status=create_user(username, password)
+    if(status == True):
+        session["id"] = find_user(username)[0][0]
+        return jsonify(success=True)
+
+    return jsonify(success=False)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json["username"]
+    password = request.json["password"]
+
+    user_id = check_user_creds(username, password)  #  Session token contains userid
+    if(len(user_id) == 0):
+        return jsonify(success = False), 401
+
+    session["id"] = user_id[0][0]
+    return jsonify(success = True)
+
+ 
 @app.route("/search", methods=["GET"])
 def search():
-    return []
+    username = request.args.get("user")
+    if(username != None):
+        return jsonify(users=find_user(username))
+    return jsonify(users=[])
+
+
+@app.route("/posts/<username>", methods=["GET"])
+def user_posts(username):
+    posts = find_user_posts(username)
+    if(posts == False):
+        return jsonify(error="User not found")
+    return jsonify(posts=posts)
+
+
+@app.route("/readpost/<post_id>", methods=["GET"])
+def read_post(post_id):
+    post = get_post_content(post_id)
+    if(post == False):
+        return jsonify(status="Not found"), 404
+
+    if "id" in session:
+        is_liked = is_post_liked(session["id"], post_id)
+    else:
+        is_liked = None
+
+    post.append(is_liked)
+    return jsonify(post)
+
+
+@app.route("/like/<post_id>", methods=["GET"])
+def like(post_id):
+    if "id" not in session:
+        return jsonify(status="Unauthorized"), 401
+
+    post = get_post_content(post_id)
+    if(post == False):
+        return jsonify(status="Not found"), 404
+
+    return jsonify(success=like_post(session["id"], post_id))
+
+
+@app.route("/post", methods=["POST"])
+def createPost():
+    if "id" not in session:
+        return jsonify(status="Unauthorized"), 401
+
+    user_id = session["id"]
+    #  Base64 encoded image is expected from frontend
+    image = request.json["image"]  #  null in JSON is set to None
+    try:
+        image = base64.b64decode(image)
+    except:
+        return jsonify(error="Bad image encoding"), 400
+
+    content = request.json["content"]
+    create_post(user_id, content, image)
+    return jsonify(success=True)
+
+
+@app.route("/follow", methods=["GET"])
+def follow():
+    return jsonify(success=True)
+
+@app.route("/feed", methods=["GET"])
+def feed():
+    return jsonify(a=1)
+
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import base64
 import hashlib
 
 USER_DB = "user.db"
@@ -31,7 +32,7 @@ sqlite_query("CREATE TABLE IF NOT EXISTS likes (postid INTEGER NOT NULL, userid 
 
 #  User database interactions functions:
 def find_user(username):
-    return sqlite_query("SELECT id, username FROM user WHERE username=?", (username,), USER_DB)
+    return sqlite_query("SELECT id, username FROM user WHERE username LIKE ?", ("%"+username+"%",), USER_DB)
 
 
 def create_user(username, password):
@@ -46,51 +47,79 @@ def create_user(username, password):
 
 def check_user_creds(username, password):
     hashpass = hashlib.sha512(password.encode()).hexdigest()
-    user = sqlite_query("SELECT * FROM user WHERE username=? AND password=?", (username, hashpass), USER_DB)
-    if(len(user) != 0):
+    return sqlite_query("SELECT id FROM user WHERE username=? AND password=?", (username, hashpass), USER_DB)
+
+
+def follow_user(user_id, target_id):
+    already_follows = sqlite_query("SELECT * FROM follows WHERE follower=? AND followed=?", (user_id, target_id), USER_DB)
+    if(len(already_follows) == 0):
+        sqlite_query("INSERT INTO follows VALUES (?, ?)", (user_id, target_id), USER_DB)
         return True
-    return False
+    return False  #  User already followed
 
 
-def follow_user(userid, target_id):
-    sqlite_query("INSERT INTO follows (?, ?)", (user_id, target_id), USER_DB)
-
-
-
-#  Binary data is expected for image
 def create_post(userid, text, image):
-    if(image == None):
+    if(image != None):
         sqlite_query("INSERT INTO post (userid, content, image, like_count) VALUES (?, ?, ?, ?)", (userid, text, image, 0), POST_DB)
     else:
         sqlite_query("INSERT INTO post (userid, content, like_count) VALUES (?, ?, ?)", (userid, text, 0), POST_DB)
     return True
 
 
-def find_user_post(username):
+def find_user_posts(username):
     #  Find user id
     user = find_user(username)
     if(len(user) == 0):
         return False
 
     userid = user[0][0]
-    posts = sqlite_query("SELECT * FROM post WHERE userid=?", (userid,), POST_DB)
-
+    posts = sqlite_query("SELECT id, content FROM post WHERE userid=?", (userid,), POST_DB)
+    
     return posts
 
 
 def get_post_content(post_id):
-    pass
+    post = sqlite_query("SELECT content, image, like_count FROM post WHERE id=?", (post_id,), POST_DB)
+    if(len(post) == 0):
+        return False
+    post = list(post[0])
+    #  Coonvert immage to b64 if not null
+    if(post[1] != None):
+        post[1] = base64.b64encode(post[1]).decode()
+    return post
 
-def like_post(userid, postid):
-    pass   
+
+def like_post(user_id, post_id):
+    if(len(sqlite_query("SELECT * FROM post WHERE id=?", (post_id,), POST_DB)) == 0):
+        return False
+
+    if(is_post_liked(user_id, post_id)):
+        return True
+    sqlite_query("INSERT INTO likes VALUES (?, ?)", (post_id, user_id), POST_DB)
+    post_likes = sqlite_query("SELECT like_count FROM post WHERE id=?", (post_id,), POST_DB)[0][0]
+    post_likes += 1
+    sqlite_query("UPDATE post SET like_count=? WHERE id=?", (post_likes, post_id), POST_DB)
+    return True
+
+
+def is_post_liked(user_id, post_id):
+    post_status = sqlite_query("SELECT * FROM likes WHERE userid=? AND postid=?", (user_id, post_id), POST_DB)
+    if(len(post_status) == 1):
+        return True
+    return False
+
 
 def post_feed(userid):
     pass
 
+
 if __name__ == "__main__":
     print(create_user("test", "pass1234"))
+    print(create_user("user2", "pass123"))
     print(find_user("test"))
     print(check_user_creds("test", "pass1234"))
     print(check_user_creds("test", "wrongpass123"))
-    print(create_post(1, "First test post!", None))
+    #print(create_post(1, "First test post!", None))
     print(find_user_post("test"))
+    follow_user(1, 2)
+
